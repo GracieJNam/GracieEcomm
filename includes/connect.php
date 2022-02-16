b9cc0134b906bd1ef5ddc3f6476e05a743a66ce6
Jungtae Ji <yamnjjt@hotmail.com>
2022-02-16 13:36:40 +1000

    db connection updated

--- includes/connect.php
+++ includes/connect.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("us-cdbr-iron-east-02.cleardb.net", "bf55ab32c56d81", "e73d326f", "heroku_020f4fd77ef9ac8"); // check connection and, if broken, display an error message
+    $con = mysqli_connect("127.0.0.1", "azure", "6#vWHD_$", "gracieecomm"); // check connection and, if broken, display an error message
     if (mysqli_connect_errno($con)) {
         echo "Unable to connect to the database: " . mysqli_connect_error();
         exit();
